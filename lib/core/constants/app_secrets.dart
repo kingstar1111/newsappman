a5139e00613bd459187faf0dab352f4669bbf6ec
kingstar1111 <47255800+kingstar1111@users.noreply.github.com>
2026-01-07 11:54:25 +0300

    Update app_secrets.dart

--- lib/core/constants/app_secrets.dart
+++ lib/core/constants/app_secrets.dart
@@ -1,6 +1,6 @@
 class AppSecrets {
-  static const String newsApiKey = '14dc2aa002c84293bcadd6667f53e970';
+  static const String newsApiKey = '###';
 
   // Get free API key from: https://aistudio.google.com/app/apikey
-  static const String geminiApiKey = 'AIzaSyC9Fb0Yjb_0zM8Vr9H75ZReTIsNVM8csr0';
+  static const String geminiApiKey = '####';
 }
